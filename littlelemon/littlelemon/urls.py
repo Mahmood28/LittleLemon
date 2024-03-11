c6ce20be7581ecf1ef69f96191c240374435307f
Mahmood28 <mahmoodalwatani@gmail.com>
2024-03-11 16:26:05 +0300

    Setup table booking api

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from restaurant.views import BookingViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r"tables", BookingViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("restaurant/booking/", include(router.urls)),
     path("restaurant/", include("restaurant.urls")),
 ]
